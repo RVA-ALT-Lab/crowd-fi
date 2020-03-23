64bf13a312057d4279ddad48a08828f4835968eb
Jeff Everhart <Jeffeverhart383@gmail.com>
2020-03-23 15:47:27 -0400

    use open street maps to avoid mapbox junk

--- js/app.js
+++ js/app.js
@@ -41,11 +41,9 @@ var MapUtilityClass = function ($) {
 
     this.initMap = function ( ) {
         var mymap = L.map('map').setView([37.5536111, -77.4605556], 14);
-        L.tileLayer('https://api.mapbox.com/styles/v1/jeffeverhart383/cj9sxi40c2g3s2skby2y6h8jh/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6IjIwNzVlOTA3ODI2MTY0MjM3OTgxMTJlODgzNjg5MzM4In0.QA1GsfWZccIB8u0FbhJmRg', {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6ImNqOXI2aDg5ejZhYncyd3M0bHd6cWYxc2oifQ.fzcb7maGkQhAxRZTotB4tg'
+            maxZoom: 18
         }).addTo(mymap);
         return mymap;
     }
