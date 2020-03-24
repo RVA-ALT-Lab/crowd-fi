685c83523b2ef6ad5171450f43d8059c32fdc104
Jeff Everhart <Jeffeverhart383@gmail.com>
2020-03-24 09:49:19 -0400

    add geolocation on map page

--- js/map-points.js
+++ js/map-points.js
@@ -2,8 +2,6 @@ var MapTool = new MapUtilityClass($);
 
 var mymap = MapTool.initMap();
 
-// L.polygon(MapTool.latlngs, { color: 'white', fillOpacity: .15 }).addTo(mymap);
-
 var mapPoints;
 MapTool.getMapPoints()
 .then(data => {
@@ -22,8 +20,3 @@ zoomButtonsArray.forEach(button => {
         mymap.flyTo([clickedPoint.meta.latitude, clickedPoint.meta.longitude])
     })
 })
-
-// var heatMapButton = document.querySelector('#toggleHeatmap')
-// heatMapButton.addEventListener('click', function(){
-//     MapTool.createHeatMapLayer(mapPoints, mymap)
-// })
--- js/map.js
+++ js/map.js
@@ -2,6 +2,18 @@ var MapTool = new MapUtilityClass(jQuery);
 
 var mymap = MapTool.initMap();
 
+MapTool.startGeolocation(mymap)
+
+var userPositionCircle = L.circle(
+    MapTool.richmondCentroid
+    ).addTo(mymap);
+
+function onLocationFound (e) {
+    userPositionCircle.setLatLng([e.latitude, e.longitude]);
+    MapTool.stopGeolocation(mymap);
+}
+
+mymap.on('locationfound', onLocationFound);
 
 MapTool.getMapPoints()
 .then(data => {
